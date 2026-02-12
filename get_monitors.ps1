be6dcaa5ec4d8cd9a3b79f2116a83988e6e0ea50
HectorLara <hector.laras18@outlook.com>
2026-02-11 18:21:20 -0600

    test

--- get_monitors.ps1
+++ get_monitors.ps1
@@ -1,7 +1,60 @@
 Import-Module OperationsManager
+# Si lo ejecutas remoto:
 # New-SCOMManagementGroupConnection -ComputerName "TU-MGMT-SERVER"
 
-Get-SCOMMonitor |
-    Select-Object DisplayName, Name, Enabled, Target, ManagementPackName |
-    Sort-Object ManagementPackName, DisplayName |
-    Export-Csv "C:\Temp\SCOM_Monitors.csv" -NoTypeInformation -Encoding UTF8
+$out = Get-SCOMMonitor | ForEach-Object {
+    $m = $_
+
+    $targetName = $null
+    $targetType = $null
+
+    try {
+        # Target suele ser una clase (ManagementPackClass)
+        $targetName = $m.Target.DisplayName
+        $targetType = "Class"
+
+        # Si el target hereda de InstanceGroup, lo marcamos como "Group"
+        # (algunos ambientes devuelven BaseTypes; si no, no truena el script)
+        if ($m.Target.BaseTypes -and ($m.Target.BaseTypes.Name -contains "Microsoft.SystemCenter.InstanceGroup")) {
+            $targetType = "Group"
+        }
+    } catch {
+        $targetName = $m.Target
+        $targetType = "Unknown"
+    }
+
+    $alertSettings = $m.AlertSettings
+
+    $generatesAlert = $false
+    $alertOnState = $null
+    $priority = $null
+    $severity = $null
+
+    if ($alertSettings) {
+        $alertOnState = $alertSettings.AlertOnState
+        $priority     = $alertSettings.AlertPriority
+        $severity     = $alertSettings.AlertSeverity
+
+        # Normalmente: si AlertOnState viene seteado, el monitor genera alerta
+        if ($alertOnState -and ($alertOnState.ToString() -notmatch "None")) {
+            $generatesAlert = $true
+        }
+    }
+
+    [pscustomobject]@{
+        DisplayName        = $m.DisplayName
+        Name               = $m.Name
+        Enabled            = $m.Enabled                 # "esta activa"
+        ManagementPack     = $m.ManagementPackName
+        Target             = $targetName                # clase / grupo target
+        TargetType         = $targetType                # Class o Group
+        GeneratesAlert     = $generatesAlert            # "genera alerta"
+        AlertOnState       = $alertOnState
+        Priority           = $priority
+        Severity           = $severity
+    }
+}
+
+$out |
+  Sort-Object ManagementPack, DisplayName |
+  Export-Csv "C:\Temp\SCOM_Monitors_Detail.csv" -NoTypeInformation -Encoding UTF8
